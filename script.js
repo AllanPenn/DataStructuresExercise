da8b232db1e6ca1e165007f18e7189da9520f183
Allan Pennycuff <allanp1238@gmail.com>
2021-09-30 09:54:40 -0500

    Completed the bonus in the script.js file.

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ const droids = {astromech: "R2-D2", protocol: "C-3P0", assassin: "IG-88"};
 console.log(droids["astromech"]);
 console.log(droids.protocol);
 droids["assassin"] = "IG-11"
+console.log(droids);
+
+// BONUS
+console.log(starWarsVillains[0][6]);
+console.log(sithLords.slice(-2, -1));
+const starWarsMovies = [
+  {episodeOne: "The Phantom Menace", episodeTwo: "Attack of the Clones", episodeThree: "Revenge of the Sith"},
+  {episodeFour: "A New Hope", episodeFive: "The Empire Strikes Back", episodeSix: "Return of the Jedi"},
+  {episodeSeven: "The Force Awakens", episodeEight: "The Last Jedi", episodeNine: "The Rise of Skywalker"}
+];
+starWarsMovies.splice(1, 0, "Solo", "Rogue One");
